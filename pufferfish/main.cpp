e1062ede800a592c5c352cf383e7b971a26c9ce6
Selyss <99344963+Selyss@users.noreply.github.com>
2025-11-14 22:36:36 -0500

    entrypoint, fen passthrough

--- pufferfish/main.cpp
+++ pufferfish/main.cpp
@@ -1,6 +1,8 @@
 // Minimal runner: initialize engine, run a short search from startpos.
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "engine/types.h"
 #include "engine/bitboard.h"
@@ -12,23 +14,69 @@
 
 using namespace pf;
 
-// struct DummyNN : NNEvaluator
-// {
-//     int evaluate(const Position &pos) override
-//     {
-//         // Very crude material-only eval as a stand-in for NN.
-//         static const int val[PIECE_NB] = {
-//             0,
-//             100, 320, 330, 500, 900, 0,
-//             -100, -320, -330, -500, -900, 0};
-//         int score = 0;
-//         for (int sq = 0; sq < 64; ++sq)
-//             score += val[pos.board[sq]];
-//         return (pos.side_to_move == WHITE ? score : -score);
-//     }
-// };
+struct MaterialEvaluator : NNEvaluator
+{
+    int evaluate(const Position &pos) override
+    {
+        static const int val[PIECE_NB] = {
+            0,
+            100, 320, 330, 500, 900, 0,
+            -100, -320, -330, -500, -900, 0};
+        int score = 0;
+        for (int sq = 0; sq < 64; ++sq)
+            score += val[pos.board[sq]];
+        return (pos.side_to_move == WHITE ? score : -score);
+    }
+};
 
-int main()
+static std::string sq_to_str(int sq)
+{
+    const char files[] = "abcdefgh";
+    std::string s;
+    s += files[sq & 7];
+    s += char('1' + (sq >> 3));
+    return s;
+}
+
+static char promo_char_from_piece(int promoPiece)
+{
+    int typeIdx = 0;
+    if (promoPiece >= W_PAWN && promoPiece <= W_KING)
+        typeIdx = promoPiece - W_PAWN;
+    else if (promoPiece >= B_PAWN && promoPiece <= B_KING)
+        typeIdx = promoPiece - B_PAWN;
+    else
+        return '\0';
+    switch (typeIdx)
+    {
+    case KNIGHT:
+        return 'n';
+    case BISHOP:
+        return 'b';
+    case ROOK:
+        return 'r';
+    case QUEEN:
+        return 'q';
+    default:
+        return '\0';
+    }
+}
+
+static std::string move_to_uci(Move m)
+{
+    int from = from_sq(m);
+    int to = to_sq(m);
+    std::string uci = sq_to_str(from) + sq_to_str(to);
+    if (move_flags(m) & FLAG_PROMOTION)
+    {
+        char pc = promo_char_from_piece(promo_piece(m));
+        if (pc)
+            uci += pc;
+    }
+    return uci;
+}
+
+int main(int argc, char **argv)
 {
     init_zobrist();
     init_bitboards();
@@ -36,24 +84,59 @@ int main()
     Position pos;
     pos.set_startpos();
 
+    // Parse CLI args: --fen <6 tokens>, --depth N, --movetime ms
+    std::string fen;
+    int depth = 5;
+    int movetime = 0;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string a = argv[i];
+        if (a == "--fen" && i + 6 <= argc)
+        {
+            std::ostringstream os;
+            os << argv[i + 1] << ' ' << argv[i + 2] << ' ' << argv[i + 3]
+               << ' ' << argv[i + 4] << ' ' << argv[i + 5] << ' ' << argv[i + 6];
+            fen = os.str();
+            i += 6;
+        }
+        else if (a == "--depth" && i + 1 < argc)
+        {
+            depth = std::max(1, std::atoi(argv[++i]));
+        }
+        else if (a == "--movetime" && i + 1 < argc)
+        {
+            movetime = std::max(0, std::atoi(argv[++i]));
+        }
+    }
+    if (!fen.empty())
+    {
+        pos.set_fen(fen);
+    }
+
     TranspositionTable tt;
     tt.resize(64); // 64 MB
 
     NNUEEvaluator nn;
-    nn.load("nnue_weights.bin");
+    bool has_nn = nn.load("nnue_weights.bin");
+    MaterialEvaluator mat;
 
     SearchContext ctx;
     ctx.tt = &tt;
-    ctx.nn = &nn;
-    ctx.limits.depth = 5;
-    ctx.limits.time_ms = 0; // depth-limited
+    ctx.nn = has_nn ? static_cast<NNEvaluator *>(&nn) : static_cast<NNEvaluator *>(&mat);
+    if (movetime > 0)
+    {
+        ctx.limits.time_ms = static_cast<std::uint64_t>(movetime);
+        ctx.limits.depth = 0;
+    }
+    else
+    {
+        ctx.limits.depth = depth;
+        ctx.limits.time_ms = 0;
+    }
 
     SearchResult res = search(pos, ctx);
 
-    std::cout << "bestmove from startpos: from " << from_sq(res.bestMove)
-              << " to " << to_sq(res.bestMove) << " score " << res.score
-              << " depth " << res.depth << " nodes " << ctx.stats.nodes
-              << " qnodes " << ctx.stats.qnodes << "\n";
+    std::cout << "bestmove " << move_to_uci(res.bestMove) << "\n";
 
     return 0;
 }
